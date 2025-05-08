1f09492a33614e7a7a182bb5a0a64c921ec876d9
mfsalop <mf.sanlop@gmail.com>
2025-05-08 13:21:11 -0500

    Refactor test setup and global search functionality; update package dependencies and improve test reporting

--- pages/globalSearch.page.js
+++ pages/globalSearch.page.js
@@ -8,32 +8,49 @@ class GlobalSearchPage {
         this.globalSearchButton = By.css('button[class="top-bar__button aux-search-button"]');
         this.globalSearchInput = By.css('input[name="search-input"]');
         this.globalSearchTitle = By.css('li[class="mint-breadcrumb__item mint-breadcrumb__item--active ng-star-inserted"]');
+        this.globalSearchFilter = By.css('div[class="search-bar__filter ng-star-inserted"]');
+        this.filterByProjectOption = By.xpath('(//mint-checkbox)[2]');
         this.cellValueSpan = By.css('a[class="ng-star-inserted"]');
     }
 
     // Start global search
     async openGlobalSearch() {
-        await this.driver.wait(until.elementLocated(this.globalSearchButton), 10000);
+        await this.driver.wait(until.elementLocated(this.globalSearchButton), 5000);
         const button = await this.driver.findElement(this.globalSearchButton);
         await button.click();
         console.log('Global search button clicked.');
 
-        await this.driver.wait(until.elementLocated(this.globalSearchInput), 10000);
-        const input = await this.driver.findElement(this.globalSearchInput);
-        await this.driver.wait(until.elementIsEnabled(input), 10000);
-        await input.clear();
-        await input.sendKeys('Visionary Expo');
-        await this.driver.sleep(5000); // Wait to ensure the input is ready
-        await input.sendKeys(Key.ENTER);
+    }
 
-        console.log('Entered search term: Visionary Expo and pressed Enter');
+    // Click on the filter button & select "Filter by Project"
+    async clickFilterButton() {
+        await this.driver.wait(until.elementLocated(this.globalSearchFilter), 1000);
+        const filterButton = await this.driver.findElement(this.globalSearchFilter);
+        await filterButton.click();
+        console.log('Filter button clicked.');
+        //await this.driver.sleep(2000); // Wait to ensure the filter dropdown is visible
+        await this.driver.wait(until.elementLocated(this.filterByProjectOption), 1000);
+        const filterOption = await this.driver.findElement(this.filterByProjectOption);
+        await filterOption.click();
+        console.log('Filter by project selected.');
+    }
+
+    // Enter search term and press Enter
+    async enterSearchTerm(searchTerm) {
+        await this.driver.wait(until.elementLocated(this.globalSearchInput), 1000);
+        const input = await this.driver.findElement(this.globalSearchInput);
+        await this.driver.wait(until.elementIsEnabled(input), 1000);
+        await input.clear();
+        await input.sendKeys(searchTerm);
+        console.log(`Entered search term: ${searchTerm}`);
+        await input.sendKeys(Key.ENTER);
     }
 
     // Verify result
     async verifyResult() {
         // Wait for the title to be visible
         await this.driver.sleep(5000);
-        await this.driver.wait(until.elementLocated(this.globalSearchTitle), 30000);
+        await this.driver.wait(until.elementLocated(this.globalSearchTitle), 1000);
         const title = await this.driver.findElement(this.globalSearchTitle);
         const titleText = await title.getText();
         console.log('Search result title:', titleText);
@@ -45,7 +62,7 @@ class GlobalSearchPage {
         }
 
         // Wait for the cell value to become visible
-        await this.driver.wait(until.elementLocated(this.cellValueSpan), 10000);
+        await this.driver.wait(until.elementLocated(this.cellValueSpan), 1000);
         const cellValueElement = await this.driver.findElement(this.cellValueSpan);
         const cellValueText = await cellValueElement.getText();
         console.log('Cell value:', cellValueText);
